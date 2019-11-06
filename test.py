96d56a772bc9d89c1b95b59d515f9dbf5bb3a7f0
FrancescaGreco <F.Greco@tudelft.nl>
2019-11-06 14:08:26 +0100

    calculation of circumference and area of a circle

--- test.py
+++ test.py
@@ -4,5 +4,10 @@ print('hello world')
 
 def circumference(r):
 	circumference = 2 * 3.14 *r
+	print(circumference)
 	return circumference
 
+def area(r):
+	area = 3.14 * r**2
+	print(area)
+	return area
